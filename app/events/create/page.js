c486a137243a1336ad7a8373b2d43794df699f9d
MANOHAR KANDULA <manoharkandula@MANOHARs-MacBook-Air.local>
2025-12-20 17:58:39 +0530

    removed one line

--- app/events/create/page.js
+++ app/events/create/page.js
@@ -1,6 +1,5 @@
 'use client'
 import { useState, useEffect } from 'react'
-import { useRouter } from 'next/navigation'
 import { ArrowLeft, Calendar, MapPin, AlignLeft, Image as ImageIcon, Type } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 
